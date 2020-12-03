17eff5eb74218e47c9113799a8ccda4f8f395535
Stuti Dobhal <stuti@7mind.de>
2020-12-03 15:25:59 +0100

    Initial Commit

---
+++ WidgetTutorial/WidgetTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTutorialApp.swift
+//  WidgetTutorial
+//
+//  Created by Stuti Dobhal on 03.12.20.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
